dd915e07a5cd4e32073775e83cccf03e5fe0d5bc
Joe Yeung <jyyeung@jyyeung-mac.lan>
2022-04-04 12:36:30 -0400

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumecounter';
+const functionApiUrl = 'https://getresumecounterjoe.azurewebsites.net/api/GetResumecounter?code=0UGVhTx3baARaXh3HsjvyRR8/MzsLEAZ/iRYi9zza5hiaOpeTnhaUw==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumecounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(Response => {
+    fetch(functionApiUrl).then(Response => {
         return Response.json()
     }).then(respone => {
         console.log("Website called function API.");
